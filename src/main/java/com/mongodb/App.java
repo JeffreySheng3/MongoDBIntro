a718c5beb661493b09259017d2c73c8d98d43e71
Jeffrey Sheng <jeffreysheng99@gmail.com>
2021-05-13 19:14:54 -0400

    All CRUD operations.

--- src/main/java/com/mongodb/App.java
+++ src/main/java/com/mongodb/App.java
@@ -1,14 +1,18 @@
 package com.mongodb;
 
 import com.mongodb.client.*;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import org.bson.Document;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Updates.*;
 
 public class App
 {
@@ -20,14 +24,38 @@ public class App
             //printDatabases(mongoClient)
             deleteDocuments(cookies);
             createDocuments(cookies);
+            updateDocuments(cookies);
+            findDocuments(cookies);
         }
     }
 
+    private static void findDocuments(MongoCollection<Document> cookies) {
+        System.out.println("Find Operations");
+        List<Document> lowCaloriesCookies = cookies.find(lte("calories", 500)).into(new ArrayList<>());
+        lowCaloriesCookies.forEach(d -> System.out.println(d.toJson()));
+    }
+
+    private static void updateDocuments(MongoCollection<Document> cookies) {
+        System.out.println("Update Operations");
+        Random random = new Random();
+        cookies.updateMany(new Document(), set("calories", random.nextInt(1000)));
+
+        List<Document> cookiesList = cookies.find().into(new ArrayList<>());
+        cookiesList.forEach(c -> {
+            Object id = c.get("_id");
+            Document cookie = cookies.findOneAndUpdate(new Document("_id", id), set("calories", random.nextInt(1000)));
+//            System.out.println(cookie.toJson());
+        });
+    }
+
     private static void deleteDocuments(MongoCollection<Document> cookies) {
-        cookies.deleteMany(in("color", List.of("blue", "orange")));
+        System.out.println("Delete Operations");
+//        cookies.deleteMany(in("color", List.of("blue", "orange")));
+        cookies.deleteMany(new Document());
     }
 
     private static void createDocuments(MongoCollection<Document> cookies) {
+        System.out.println("Create Operations");
         List<Document> cookiesList = new ArrayList<>();
         List<String> ingredients = List.of("flour", "eggs", "butter", "sugar", "red food coloring");
 
